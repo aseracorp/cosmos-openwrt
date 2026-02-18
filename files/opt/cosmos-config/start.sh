b186d8b0f617c9a3abe64fccd1ab317ffb84cddc
madejackson <martinra1994@gmail.com>
2026-02-18 11:40:39 +0100

    improve logic for co-processor

--- files/opt/cosmos-config/start.sh
+++ files/opt/cosmos-config/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>&1 2>&1
+# Everything below will go to logread:
+
+
 
 #set LED's to normal mode, startup
 cd /opt/cosmos-config && ./init.sh
